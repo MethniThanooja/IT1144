e8324ef5bc4aa22a4c46852b0bdfbeef4519cce6
MethniThanooja <methnithanooja0224@gmail.com>
2026-06-06 06:37:30 +0530

    Universiry of vavuniya website

---
+++ uov website/website/login,home.js
@@ -0,0 +1,7 @@
+document.getElementById("signinBtn").addEventListener("click",function(){
+	alert("welcome!");
+});
+
+document.getElementById("cancel").addEventListener("click",function(){
+	alert("Are you sure? Do you want to cancel?");
+});
---
+++ uov website/website/uov.js
@@ -0,0 +1,7 @@
+document.getElementById("signinBtn").addEventListener("click",function(){
+	alert("You click the sign in button!");
+});
+
+document.getElementById("loginBtn").addEventListener("click",function(){
+	alert("You clicked the login button!");
+});
